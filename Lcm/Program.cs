b3329d833883454756a7bc980d440e0214e8c090
udonk <6565941+curryudon08@users.noreply.github.com>
2020-03-29 02:45:31 +0900

    コードを追加
    最小公倍数、最大公約数を求めるコードを追加

---
+++ Lcm/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lcm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _ = Console.ReadLine().Split();
+            var a = int.Parse(_[0]);
+            var b = int.Parse(_[1]);
+
+            Console.WriteLine(Lcm(a, b));
+        }
+
+        //最小公倍数
+        static long Lcm(long m, long n){
+        	return (m * n) / Gcd(m, n);
+        }
+
+        //最大公約数
+        static long Gcd(long m, long n)
+        {
+	        while (m % n != 0){
+        		var temp = n;
+		        n = m % n;
+		        m = temp;
+	        }
+        	return n;
+        }
+    }
+}
